c71209b622de396e05ae3471be131a2828df395a
Winslow <winslow@clearcreeksci.com>
2026-07-24 21:23:31 -0400

    try again

--- deployment/rewrite_settings.py
+++ deployment/rewrite_settings.py
@@ -9,7 +9,7 @@ SETTINGS_NAME = 'settings.cfg'
 def hash_value(v):
     # Match default flask_bcrypt values
     rounds = 12
-    prefix = '2b'.decode('utf-8')
+    prefix = '2b'.encode('utf-8')
     salt = bcrypt.gensalt(rounds,prefix)
     pw = v.encode('utf-8')
     return bcrypt.hashpw(pw,salt)
